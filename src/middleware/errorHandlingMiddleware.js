80e20bbdf55aff42240deedca9917439c94c3f77
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2024-06-27 19:25:11 +0600

    feat!: update error handling middleware to handle all error

--- src/middleware/errorHandlingMiddleware.js
+++ src/middleware/errorHandlingMiddleware.js
@@ -1,24 +1,40 @@
 import httpStatus from '../constant/statusCodes.constants.js';
 
 const errorHandlingMiddleware = (error, req, res, next) => {
-    // Handle specific error types here
-    if (error.code === 'ECONNREFUSED') {
-        console.error('Connection refused error:', error);
+    // Set a default status and message in case none is explicitly set by the throwing function
+    let { status, message } = error;
 
-        const errorData = {
-            route: req.originalUrl,
-            timeStamp: new Date(),
-            success: false,
-            data: {},
-            message: 'Service temporarily unavailable. Please try again later.',
-            status: httpStatus.SERVICE_UNAVAILABLE,
-        };
-
-        res.status(errorData.status).send(errorData);
+    // Log detailed error information in development for debugging
+    if (process.env.NODE_ENV === 'development') {
+        console.error('Error:', {
+            error,
+            body: req.body,
+            path: req.path
+        });
     }
 
-    // Pass other errors to the default error handler
-    next(error);
+    // Prepare the response object
+    const response = {
+        route: req.originalUrl,
+        timeStamp: new Date(),
+        success: false,
+        data: process.env.NODE_ENV === 'development' && { stack: error.stack },
+        message: message,
+        status: status,
+    };
+
+    // Handle known error types here
+    if (error.code === 'ECONNREFUSED') {
+        response.status = httpStatus.SERVICE_UNAVAILABLE;
+        response.message = 'Service temporarily unavailable. Please try again later.';
+    } else if (error.isOperational) {
+        response.message = error.message; // Operational error with a more user-friendly message
+    } else {
+        // For security reasons, do not send the error details to the client in production
+        response.message = process.env.NODE_ENV === 'production' ? 'An unexpected error occurred' : error.message;
+    }
+
+    res.status(response.status).json(response);
 };
 
 export default errorHandlingMiddleware;
