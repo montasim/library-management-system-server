0f4ed7e51ccf75bec9a7363d07c03dd2c6fe0f6f
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2024-07-03 00:33:58 +0600

    refactor: writers routes import name

--- src/modules/api/api.routes.js
+++ src/modules/api/api.routes.js
@@ -2,12 +2,12 @@ import express from 'express';
 
 import booksRoutes from './books/books.routes.js';
 import subjectsRoutes from './subjects/subjects.routes.js';
-import writerRoutes from './writers/writers.routes.js';
+import writersRoutes from './writers/writers.routes.js';
 
 const router = express.Router();
 
 router.use('/books', booksRoutes);
 router.use('/subjects', subjectsRoutes);
-router.use('/writers', writerRoutes);
+router.use('/writers', writersRoutes);
 
 export default router;
