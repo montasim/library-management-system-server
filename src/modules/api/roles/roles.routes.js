dc16bd94ed1b646e86030369b3595d9663de05de
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2024-07-29 20:05:50 +0600

    feat: optimized authenticateMiddleware for roles API

--- src/modules/api/roles/roles.routes.js
+++ src/modules/api/roles/roles.routes.js
@@ -2,7 +2,6 @@ import express from 'express';
 
 import methodNotSupported from '../../../shared/methodNotSupported.js';
 import routesConstants from '../../../constant/routes.constants.js';
-import authenticateMiddleware from '../../../middleware/authenticate.middleware.js';
 import rolesValidator from './roles.validator.js';
 import rolesController from './roles.controller.js';
 
@@ -11,17 +10,14 @@ const router = express.Router();
 router
     .route('/')
     .post(
-        authenticateMiddleware.admin,
         rolesValidator.createRole,
         rolesController.createRole
     )
     .get(
-        authenticateMiddleware.admin,
         rolesValidator.getRoles,
         rolesController.getRoles
     )
     .delete(
-        authenticateMiddleware.admin,
         rolesValidator.deleteRoles,
         rolesController.deleteRoles
     )
@@ -30,17 +26,14 @@ router
 router
     .route(`/:${routesConstants.roles.params}`)
     .get(
-        authenticateMiddleware.admin,
         rolesValidator.getRole,
         rolesController.getRole
     )
     .put(
-        authenticateMiddleware.admin,
         rolesValidator.updateRole,
         rolesController.updateRole
     )
     .delete(
-        authenticateMiddleware.admin,
         rolesValidator.deleteRole,
         rolesController.deleteRole
     )
