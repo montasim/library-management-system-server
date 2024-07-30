178ea2ad5d2e46176932da7e286f821b5542edaa
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2024-07-30 09:50:43 +0600

    refactor: user profile api

--- src/modules/api/userProfile/userProfile.routes.js
+++ src/modules/api/userProfile/userProfile.routes.js
@@ -2,13 +2,12 @@ import express from 'express';
 
 import methodNotSupported from '../../../shared/methodNotSupported.js';
 import userProfileController from './userProfile.controller.js';
-import authenticateMiddleware from '../../../middleware/authenticate.middleware.js';
 
 const router = express.Router();
 
 router
     .route('/:username')
-    .get(authenticateMiddleware.user, userProfileController.getProfile)
+    .get(userProfileController.getProfile)
     .all(methodNotSupported);
 
 export default router;
