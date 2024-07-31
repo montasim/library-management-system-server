097e851433cffbbc65fe07932bf9e643f4ec9bab
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2024-07-31 17:47:00 +0600

    refactor: using shared entity for role controller

--- src/modules/api/roles/roles.controller.js
+++ src/modules/api/roles/roles.controller.js
@@ -1,92 +1,14 @@
-import asyncErrorHandlerService from '../../../service/asyncErrorHandler.service.js';
+import entity from '../../../shared/entity.js';
 import rolesService from './roles.service.js';
-import getRequesterId from '../../../utilities/getRequesterId.js';
-
-const createRole = asyncErrorHandlerService(async (req, res) => {
-    const requester = getRequesterId(req);
-    const newRoleData = await rolesService.createRole(requester, req.body);
-
-    newRoleData.route = req.originalUrl;
-
-    res.status(newRoleData.status).send(newRoleData);
-});
-
-const createDefaultRole = asyncErrorHandlerService(async (req, res) => {
-    const requester = getRequesterId(req);
-    const newRoleData = await rolesService.createDefaultRole(requester);
-
-    newRoleData.route = req.originalUrl;
-
-    res.status(newRoleData.status).send(newRoleData);
-});
-
-const getRoleList = asyncErrorHandlerService(async (req, res) => {
-    const requester = getRequesterId(req);
-    const rolesData = await rolesService.getRoleList(requester, req.query);
-
-    rolesData.route = req.originalUrl;
-
-    res.status(rolesData.status).send(rolesData);
-});
-
-const getRoleById = asyncErrorHandlerService(async (req, res) => {
-    const requester = getRequesterId(req);
-    const roleData = await rolesService.getRoleById(
-        requester,
-        req.params.roleId
-    );
-
-    roleData.route = req.originalUrl;
-
-    res.status(roleData.status).send(roleData);
-});
-
-const updateRoleById = asyncErrorHandlerService(async (req, res) => {
-    const requester = getRequesterId(req);
-    const updatedRoleData = await rolesService.updateRoleById(
-        requester,
-        req.params.roleId,
-        req.body
-    );
-
-    updatedRoleData.route = req.originalUrl;
-
-    res.status(updatedRoleData.status).send(updatedRoleData);
-});
-
-const deleteRoleByList = asyncErrorHandlerService(async (req, res) => {
-    const requester = getRequesterId(req);
-    const roleIds = req.query.ids.split(',');
-    const deletedRolesData = await rolesService.deleteRoleByList(
-        requester,
-        roleIds
-    );
-
-    deletedRolesData.route = req.originalUrl;
-
-    res.status(deletedRolesData.status).send(deletedRolesData);
-});
-
-const deleteRoleById = asyncErrorHandlerService(async (req, res) => {
-    const requester = getRequesterId(req);
-    const deletedRoleData = await rolesService.deleteRoleById(
-        requester,
-        req.params.roleId
-    );
-
-    deletedRoleData.route = req.originalUrl;
-
-    res.status(deletedRoleData.status).send(deletedRoleData);
-});
 
 const rolesController = {
-    createRole,
-    createDefaultRole,
-    getRoleList,
-    getRoleById,
-    updateRoleById,
-    deleteRoleByList,
-    deleteRoleById,
+    createRole: entity.createEntity(rolesService, 'createRole'),
+    createDefaultRole: entity.createEntity(rolesService, 'createDefaultRole'),
+    getRoleList: entity.getEntityList(rolesService, 'getRoleList'),
+    getRoleById: entity.getEntityById(rolesService, 'getRoleById'),
+    updateRoleById: entity.updateEntityById(rolesService, 'updateRoleById'),
+    deleteRoleById: entity.deleteEntityById(rolesService, 'deleteRoleById'),
+    deleteRoleByList: entity.deleteEntityList(rolesService, 'deleteRoleList'),
 };
 
 export default rolesController;
