8519c2efa563ae500bb2d6f3245b2f082e8e461d
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2024-08-01 02:15:14 +0600

    feat: set default image for user and admin

--- src/constant/default.constants.js
+++ src/constant/default.constants.js
@@ -1,7 +1,7 @@
 const images = {
     user: {
-        male: 'https://drive.google.com/file/d/1YvPc76F0FoDvJSws6rdGPxBnYvd3icHZ/view?usp=drive_link',
-        female: 'https://drive.google.com/file/d/1yJN6Im5E2aAF9hFIzn38DFvfbreFng4f/view?usp=drive_link',
+        male: 'https://drive.google.com/uc?export=view&id=1YvPc76F0FoDvJSws6rdGPxBnYvd3icHZ',
+        female: 'https://drive.google.com/uc?export=view&id=1yJN6Im5E2aAF9hFIzn38DFvfbreFng4f',
     }
 };
 
