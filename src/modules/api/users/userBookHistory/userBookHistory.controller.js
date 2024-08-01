0982ff7ee2b4de9baea6a3251164e90f0f51ca28
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2024-08-01 13:28:56 +0600

    refactor: use entity for user book history and request books

--- src/modules/api/users/userBookHistory/userBookHistory.controller.js
+++ src/modules/api/users/userBookHistory/userBookHistory.controller.js
@@ -1,33 +1,16 @@
-import asyncErrorHandlerService from '../../../../service/asyncErrorHandler.service.js';
-import getRequesterId from '../../../../utilities/getRequesterId.js';
 import userBookHistoryService from './userBookHistory.service.js';
-
-const getBooksHistory = asyncErrorHandlerService(async (req, res) => {
-    const requester = getRequesterId(req);
-    const booksHistoryData =
-        await userBookHistoryService.getBooksHistory(requester);
-
-    booksHistoryData.route = req.originalUrl;
-
-    res.status(booksHistoryData.status).send(booksHistoryData);
-});
-
-const getBookHistoryByBookId = asyncErrorHandlerService(async (req, res) => {
-    const requester = getRequesterId(req);
-    const booksHistoryData =
-        await userBookHistoryService.getBookHistoryByBookId(
-            requester,
-            req.params.bookId
-        );
-
-    booksHistoryData.route = req.originalUrl;
-
-    res.status(booksHistoryData.status).send(booksHistoryData);
-});
+import entity from '../../../../shared/entity.js';
 
 const userBookHistoryController = {
-    getBooksHistory,
-    getBookHistoryByBookId,
+    getBooksHistory: entity.getEntityByRequester(
+        userBookHistoryService,
+        'getBooksHistory'
+    ),
+    getBookHistoryByBookId: entity.getEntityById(
+        userBookHistoryService,
+        'getBookHistoryByBookId',
+        'bookId'
+    ),
 };
 
 export default userBookHistoryController;
--- src/modules/api/users/userRequestBooks/userRequestBooks.controller.js
+++ src/modules/api/users/userRequestBooks/userRequestBooks.controller.js
@@ -1,45 +1,21 @@
-import asyncErrorHandlerService from '../../../../service/asyncErrorHandler.service.js';
 import requestBooksService from './userRequestBooks.service.js';
-import getRequesterId from '../../../../utilities/getRequesterId.js';
-
-const getRequestBooks = asyncErrorHandlerService(async (req, res) => {
-    const requester = getRequesterId(req);
-    const requestBooksData =
-        await requestBooksService.getRequestBooks(requester);
-
-    requestBooksData.route = req.originalUrl;
-
-    res.status(requestBooksData.status).send(requestBooksData);
-});
-
-const getRequestBook = asyncErrorHandlerService(async (req, res) => {
-    const requester = getRequesterId(req);
-    const requestBooksData = await requestBooksService.getRequestBook(
-        requester,
-        req.params.requestedBookId
-    );
-
-    requestBooksData.route = req.originalUrl;
-
-    res.status(requestBooksData.status).send(requestBooksData);
-});
-
-const deleteRequestBook = asyncErrorHandlerService(async (req, res) => {
-    const requester = getRequesterId(req);
-    const deletedRequestBookData = await requestBooksService.deleteRequestBook(
-        requester,
-        req.params.requestedBookId
-    );
-
-    deletedRequestBookData.route = req.originalUrl;
-
-    res.status(deletedRequestBookData.status).send(deletedRequestBookData);
-});
+import entity from '../../../../shared/entity.js';
 
 const userRequestBooksController = {
-    getRequestBooks,
-    getRequestBook,
-    deleteRequestBook,
+    getRequestBooks: entity.getEntityByRequester(
+        requestBooksService,
+        'getRequestBooks'
+    ),
+    getRequestBook: entity.getEntityById(
+        requestBooksService,
+        'getRequestBook',
+        'requestedBookId'
+    ),
+    deleteRequestBook: entity.deleteEntityById(
+        requestBooksService,
+        'deleteRequestBook',
+        'requestedBookId'
+    ),
 };
 
 export default userRequestBooksController;
