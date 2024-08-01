eb3f5f023e0175ee7065bee555be70ff45565da3
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2024-08-01 11:26:03 +0600

    feat: add node cache for home routes

--- src/modules/index/home/home.routes.js
+++ src/modules/index/home/home.routes.js
@@ -2,9 +2,17 @@ import express from 'express';
 
 import homeController from './home.controller.js';
 import methodNotSupported from '../../../shared/methodNotSupported.js';
+import cacheMiddleware from '../../../middleware/cache.middleware.js';
+import configuration from '../../../configuration/configuration.js';
 
 const router = express.Router();
 
-router.route('/').get(homeController).all(methodNotSupported);
+router
+    .route('/')
+    .get(
+        homeController,
+        cacheMiddleware.create(configuration.cache.timeout),
+    )
+    .all(methodNotSupported);
 
 export default router;
