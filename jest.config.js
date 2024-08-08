0595bd489259b7b7f6cb51b9348ebd42dae77625
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2024-08-08 23:25:56 +0600

    refactor: add jest config description

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,23 @@
+/**
+ * Jest Configuration File
+ * @fileoverview This configuration file sets up Jest for testing JavaScript and TypeScript projects.
+ * It includes settings for transforming files with Babel and configuring the test environment.
+ */
+
 export default {
+    /**
+     * Transform settings for Jest.
+     * @type {Object}
+     * @property {string} '\\.[jt]sx?$' - Regex pattern for matching JavaScript and TypeScript files.
+     * @property {string} 'babel-jest' - Transformer module to use for transforming the matched files.
+     */
     transform: {
         '\\.[jt]sx?$': 'babel-jest',
     },
+
+    /**
+     * Specifies the test environment that will be used for testing.
+     * @type {string}
+     */
     testEnvironment: 'node',
 };
